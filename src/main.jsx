6b57c25bd9f223a12301d3b73d2fb5ed8c495348
daw2-garcia23 <garciamanjonjoel@fpllefia.com>
2024-05-17 17:39:16 +0200

    .

--- src/main.jsx
+++ src/main.jsx
@@ -3,17 +3,12 @@ import ReactDOM from 'react-dom/client'
 import {NextUIProvider} from '@nextui-org/react'
 import App from './App.jsx'
 import './index.css'
-import { Card } from './Componentes/Card.jsx'
- 
-
 
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
-    <Card></Card>
     <NextUIProvider>
-      <main className="purple-dark text-foreground bg-background">
+      <main className="purple-dark text-foreground">
         <App />
       </main>
     </NextUIProvider>
