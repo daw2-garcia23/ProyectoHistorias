3ac6456363e31eac6a9e2eb9a7901dc7a92e3596
daw2-garcia23 <garciamanjonjoel@fpllefia.com>
2024-05-29 20:55:52 +0200

    Historia 4 acabada

--- src/Componentes/Cards.jsx
+++ src/Componentes/Cards.jsx
@@ -1,26 +1,18 @@
-import { useContext } from "react";
+import React, { useContext, useState } from "react";
 import { DataContext } from "./GlobalContext";
 import { Button } from "@nextui-org/react";
 import { Plus } from "lucide-react";
-import ModalForm from "./ModalForm";
+import ModalFormAdd from "./ModalAdd"; 
 import { Carta } from "./Card";
-
-
 
 export function Cartas() {
   const { data, setData } = useContext(DataContext);
+  const [ModalAbierto, setAbierto] = useState(false);
 
-  const AgregarCarta = () => {
-    const nuevaCarta = {
-      id: data.length + 1,
-      titulo: "Nuevo Título",
-      fecha: "Nueva Fecha",
-      experiencia: "Nueva Experiencia",
-      imagen: "https://blog.localadventures.mx/wp-content/uploads/2023/04/hermosas-montanas-ratchaprapha-dam-parque-nacional-khao-sok-provincia-surat-thani-tailandia-scaled.jpg",
-    };
-    setData([...data, nuevaCarta]);
-  };
+  const abrirModal = () => setAbierto(true);
+  const cerrarModal = () => setAbierto(false);
 
+ 
   return (
     <div>
       <div className="grid grid-cols-4 gap-4 ">
@@ -37,12 +29,12 @@ export function Cartas() {
         ))}
       </div>
       <div className="bg-success w-[60px] h-[60px] rounded-full fixed bottom-5 right-5 flex items-center justify-center mt-2">
+        <ModalFormAdd isOpen={ModalAbierto} onClose={cerrarModal} /> 
         <Button
           className="text-white text-3xl"
           color="success"
           size="sm"
-          onClick={AgregarCarta}
-          onPress={ModalForm}
+          onPress={abrirModal}
         >
           <Plus />
         </Button>
